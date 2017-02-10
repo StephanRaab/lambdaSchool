f386922a41b9997550aa6bbe19267440a725fef7
StephanRaab <sarcartist@gmail.com>
2017-02-09 22:14:24 -0500

    lambdaschool homework lesson#2

---
+++ lesson2/myBlog.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('home.html')
+
+@app.route("/birthday")
+def birthday():
+    return 'October 31 1989'
+
+@app.route("/greeting/<name>")
+def greeting(name):
+    return 'Hello ' + name
+
+@app.route("/sum/<one>/<two>")
+def sum(one,two):
+    return one +  ' + ' +  two + ' = ' + str(int(one)+int(two))
+
+@app.route("/multiply/<one>/<two>")
+def multiply(one,two):
+    return one +  ' * ' +  two + ' = ' + str(int(one)*int(two))
+
+@app.route("/subtract/<one>/<two>")
+def subtract(one,two):
+    return one +  ' - ' +  two + ' = ' + str(int(one)-int(two))
+
+@app.route("/favoritefoods")
+def favoritefoods():
+    return jsonify(['French Fries', 'Soup', 'Oatmeal'])
